db1e670a6dbbb2e0b124563fe7e64f02ac37406b
Abdulaziz1920 <abdulazizgit1920@gmail.com>
2023-07-15 03:58:01 +0300

    project finished

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -5,19 +5,19 @@ import "../sass/pages/_content.scss";
 const Content = () => {
   const repost = [
     {
-      image: "/src/assets/car.png",
+      image: "/public/car.png",
       title: "How to Drive a Car Safely",
       subtitle:
         "Ah, the joy of the open road—it’s a good feeling. But if you’re new to driving, you may feel a little nervous about getting behind the wheel. Don’t worry. While it’s true that accidents can happen to anybody, there are things you can do to drive safely and do your best to avoid them. ",
     },
     {
-      image: "/src/assets/girl.png",
+      image: "/public/girl.png",
       title: "How to Make Dance Music",
       subtitle:
         "Download torrents from verified or trusted uploaders. If you're a BitTorrent user looking for safety tips, use this method. Both of the big-name BitTorrent indexers (The Pirate Bay and KickAssTorrents) use symbols to highlight torrents uploaded by verified users. ",
     },
     {
-      image: "/src/assets/pc.png",
+      image: "/public/pc.png",
       title: "Why I Stopped Using Multiple Monitor",
       subtitle:
         "A Single Monitor Manifesto — Many developers believe multiple monitors improve productivity. Studies have proven it, right? Well, keep in mind, many of those studies are commissioned from monitor manufacturers like ",
